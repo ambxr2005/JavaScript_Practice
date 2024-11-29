38bf91d251099c5168e8ba9d2f1b1dcfe575739e
Aman Kumar <ambxr2005@gmail.com>
2024-11-30 00:40:43 +0530

    UPDATED 30/11/2024
    Started learning JavaScript and preparing for GSoC 2025!

---
+++ Codes/Chapter_1.js
@@ -0,0 +1,64 @@
+console.log("Hello, Aman!");
+console.log("How are you?");
+console.log("Let's start learning JavaScript!");
+
+age = 32;
+fullName = "Tony Stark";
+price = 99.99;
+console.log(fullName);
+console.log(age);
+console.log(price);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+isFollowing = false;
+console.log(isFollowing);
+
+const PI = 3.14;
+console.log(PI);
+
+{
+    let a = 10;
+    console.log(a);
+}
+
+{
+    let a = 3;
+    console.log(a);
+}
+
+const student = {    // this is an onject
+    fullName : "Rahul Kumar",
+    age : 20,
+    cgpa : 8.9,
+    isPass : true
+};
+
+// Both formats can be used:
+console.log(student.fullName);
+console.log(student["age"]);
+
+const product = {
+    title : "Parker Jotter Standard CT Ball Pen (Black)",
+    rating : 4,
+    mrp : 285,
+    deal : true,
+    offer : 5,
+    price : 270
+};
+console.log("Data of Product:");
+console.log(product);
+
+const profile = {
+    username : "shradhakhapra",
+    posts : 195,
+    followers : 569000,
+    following : 4,
+    isFollowed : false
+};
+console.log("Profile Data:");
+console.log(profile);
